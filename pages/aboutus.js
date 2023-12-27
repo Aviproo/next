339b3e877c62425609cba207c46182190081ffb2
AVINASH KUMAR <Aviproo>
2023-12-27 11:20:12 +0530

    Next js

---
+++ pages/aboutus.js
@@ -0,0 +1,4 @@
+function AboutUs() {
+  return <h1>This is about us page</h1>;
+}
+export default AboutUs;
---
+++ pages/news.js
@@ -0,0 +1,5 @@
+function NewsPage() {
+  return <h1>this is news page</h1>;
+}
+
+export default NewsPage;
